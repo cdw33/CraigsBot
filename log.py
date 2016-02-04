ba888a918e3c437fad92b72d126c50666bba7f2c
cdw33 <cdw33@zips.uakron.edu>
2016-02-04 16:22:43 -0500

    Added functions to scrape data from Craigslist listing

--- log.py
+++ log.py
@@ -2,22 +2,22 @@ import logging
 
 class Logger:
 
-   logging.basicConfig(level=logging.INFO)
-   log = logging.getLogger(__name__)
-
-   # create a file handler
-   handler = logging.FileHandler('.log')
-   handler.setLevel(logging.INFO)
-
-   # create a logging format
-   formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-   handler.setFormatter(formatter)
-
-   # add the handlers to the logger
-   log.addHandler(handler)
+   log = None   
 
    def __init__(self):
-      None
+      logging.basicConfig(level=logging.INFO)
+      log = logging.getLogger(__name__)
+
+      # create a file handler
+      handler = logging.FileHandler('.log')
+      handler.setLevel(logging.INFO)
+
+      # create a logging format
+      formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+      handler.setFormatter(formatter)
+
+      # add the handlers to the logger
+      log.addHandler(handler)
 
    def i(self, message):
    	self.log.info(message)
